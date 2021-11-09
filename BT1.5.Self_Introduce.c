2cee5f8767f5d1832c276934d8f5e94f30502362
Pham Quang Khoi <69102125+khoishady@users.noreply.github.com>
2021-11-09 14:37:19 +0700

    Add files via upload

---
+++ BT1.5.Self_Introduce.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n Chao PHENIKAA UNIVERSITY!");
+    printf("\n  Ten toi la: Nguyen Van A");
+    printf("\n  Gioi tinh: Nam");
+    printf("\n  Que quan: Ha Noi");
+    printf("\n Toi la sinh vien lop CNTT Viet-Nhat K13");
+}
